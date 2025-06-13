cce27dc91f03a20522c6b988d0166604afd78bc2
Yvonne <ys359375@gmail.com>
2025-06-13 12:26:31 +0300

    .

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
+from sqlalchemy import (Column, String, Integer, create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 
-def create_table(base):
-    pass
+Base = declarative_base()
+
+def create_table(base, engine):
+    Base.metadata.create_all(engine)
+
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name ==name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    for d in session.query(Dog):
+        d.dog = dog
+        d.breed = breed
